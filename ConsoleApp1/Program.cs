592fffc005793bf1dbfc7ef43992467a7f98b6c7
Serge042 <sakovtun412@gmail.com>
2025-01-15 17:57:11 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿class MainClass
+{
+    public static void Main(string[] args)
+    {
+
+        string myName;
+        myName = "Serge";
+
+        Console.WriteLine(myName);
+
+        Console.ReadKey();
+    }
+}
